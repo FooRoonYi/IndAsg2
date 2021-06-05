51e182b749e7547caed88da54b5ef2eacb620224
Foo Roon Yi <80664992+FooRoonYi@users.noreply.github.com>
2021-06-05 20:03:09 +0800

    Delete indAsg2 directory

--- indAsg2/Discount.java
+++
@@ -1,8 +0,0 @@
-package indAsg2;
-
-//2.5 Interface
-public interface Discount { //this super class is interface where automatically defined abstract
-	
-	double discountRate(); //this method also automatically abstract
-	
-}
